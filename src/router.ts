9f5291b7c4eccede513675ca78ab984caa939211
dev <madisongenevieve@live.com>
2019-05-27 16:21:38 -0500

    fixing sw

--- src/router.ts
+++ src/router.ts
@@ -17,7 +17,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      alias: 'About E',
+      alias: 'About Em',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
